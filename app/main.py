94d2b600a387257b8b86cf202b937bdd70b8a7e9
Sasha <kluzodota@gmail.com>
2025-05-28 22:28:50 +0300

    fix first problem

--- app/main.py
+++ app/main.py
@@ -5,13 +5,14 @@ def cache(func: Callable) -> Callable:
     data = {}
 
     def inner(*args, **kwargs) -> int:
-        if args in data:
+        if args in data and tuple(kwargs.values()) in data:
             print("Getting from cache")
             return data[args]
         else:
             print("Calculating new result")
             res = func(*args, **kwargs)
             data[args] = res
+            data[tuple(kwargs.values())] = res
             return res
 
     return inner
