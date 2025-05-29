381dbb2ecd4d4c6398d47ac59532c2f3eeb8592f
Sasha <kluzodota@gmail.com>
2025-05-29 16:13:32 +0300

    second attempt

--- app/main.py
+++ app/main.py
@@ -1,18 +1,32 @@
-from typing import Callable
+from typing import Callable, List, Any
 
 
 def cache(func: Callable) -> Callable:
     data = {}
 
     def inner(*args, **kwargs) -> int:
-        if args in data and tuple(kwargs.values()) in data:
+        key = (args, tuple(sorted(kwargs.items())))
+        if key in data:
             print("Getting from cache")
-            return data[args]
+            return data[key]
         else:
             print("Calculating new result")
             res = func(*args, **kwargs)
-            data[args] = res
-            data[tuple(kwargs.values())] = res
+            data[key] = res
             return res
 
     return inner
+
+
+@cache
+def long_time_func(a: int, b: int, c: int) -> int:
+    return (a ** b ** c) % (a * c)
+
+
+@cache
+def long_time_func_2(n_tuple: tuple, power: int) -> list[Any]:
+    return [number ** power for number in n_tuple]
+
+
+long_time_func(1, 2, 3)
+long_time_func(1, 2, c=3)
